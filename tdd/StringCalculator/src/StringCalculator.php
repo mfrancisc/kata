765e4e902dcc1151c465067ff7ed09b53486006b
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-06-06 01:00:43 +0200

    Format code

--- tdd/StringCalculator/src/StringCalculator.php
+++ tdd/StringCalculator/src/StringCalculator.php
@@ -3,20 +3,16 @@ class StringCalculator
 {
     public function add($stringOfValues) {
         
-        
         $result = 0;
-        $delimiter= "[\s,]";
-    if(substr( $stringOfValues, 0, 2 ) === "//"){
-    	$delimiter = substr( $stringOfValues, 2, 1 );
-    }
-            $values = preg_split("/$delimiter/", $stringOfValues);
-            foreach ($values as $key => $value) {
-                $result+= $value;
-            }
-
-             
-           
+        $delimiter = "[\s,]";
+        if (substr($stringOfValues, 0, 2) === "//") {
+            $delimiter = substr($stringOfValues, 2, 1);
+        }
+        $values = preg_split("/$delimiter/", $stringOfValues);
+        foreach ($values as $key => $value) {
+            $result+= $value;
+        }
+        
         return $result;
     }
-
 }
