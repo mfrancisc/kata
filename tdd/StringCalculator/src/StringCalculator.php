bee94cb214139c31f2766b139119d54871d520f8
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-06-10 01:17:20 +0200

    Completed step 6,7

--- tdd/StringCalculator/src/StringCalculator.php
+++ tdd/StringCalculator/src/StringCalculator.php
@@ -1,6 +1,9 @@
 <?php
 class StringCalculator
 {
+    const SEP_START = "//[";
+    const SEP_END = "]\n";
+    const DEFAULT_SEP = "\s,";
     public $result;
     private $negNumbers;
     
@@ -10,28 +13,62 @@ class StringCalculator
     private function resetNegNumbersList() {
         $this->negNumbers = FALSE;
     }
-    private function getDefaultSeparator() {
-        return "[\s,]";
+    public function setResultStringAndSep($stringOfValues, $sep) {
+        return $result = array('sep' => $sep, 'stringOfValues' => $stringOfValues);
     }
-    private function getSeparator($stringOfValues) {
+    private function escapeDefaultSeparator($stringOfValues) {
+        return $this->setResultStringAndSep($stringOfValues, self::DEFAULT_SEP);
+    }
+    private function escapeSeparator($stringOfValues) {
         if (substr($stringOfValues, 0, 2) === "//") {
-            return substr($stringOfValues, 2, 1);
+            $sep = $this->get_string_between($stringOfValues, self::SEP_START, self::SEP_END);
+            $stringOfValues = $this->delete_all_between($stringOfValues, self::SEP_START, self::SEP_END);
+            return $this->setResultStringAndSep($stringOfValues, $sep);
         }
-        return $this->getDefaultSeparator();
+        return $this->escapeDefaultSeparator($stringOfValues);
     }
     private function getArrayValuesFromString($sep, $stringOfValues) {
-        return preg_split("/$sep/", $stringOfValues);
+        return preg_split("/[$sep]/", $stringOfValues);
+    }
+    private function isValidNumber($number) {
+        if ($number < 0) {
+            $this->negNumbers.= $number . ',';
+            return FALSE;
+        }
+        if ($number > 1000) {
+            return FALSE;
+        }
+        return TRUE;
+    }
+    public function get_string_between($string, $start, $end) {
+        $string = " " . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return "";
+        $ini+= strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
+    function delete_all_between($string, $beginning, $end) {
+        $beginningPos = strpos($string, $beginning);
+        $endPos = strpos($string, $end);
+        if ($beginningPos === false || $endPos === false) {
+            return $string;
+        }
+        
+        $textToDelete = substr($string, $beginningPos, ($endPos + strlen($end)) - $beginningPos);
+        
+        return str_replace($textToDelete, '', $string);
     }
     public function add($stringOfValues) {
         $this->resetResult();
-        $sep = $this->getSeparator($stringOfValues);
+        $result = $this->escapeSeparator($stringOfValues);
         
         $this->resetNegNumbersList();
-        foreach ($this->getArrayValuesFromString($sep, $stringOfValues) as $key => $value) {
-            if ($value < 0) $this->negNumbers.= $value . ',';
-            $this->result+= $value;
+        foreach ($this->getArrayValuesFromString($result['sep'], $result['stringOfValues']) as $key => $number) {
+            
+            if ($this->isValidNumber($number)) $this->result+= $number;
         }
-        if ($this->negNumbers) throw new InvalidArgumentException('negatives not allowed ' . $this->negNumbers);
+        if ($this->negNumbers !== FALSE) throw new InvalidArgumentException('negatives not allowed ' . $this->negNumbers);
         
         return $this->result;
     }
--- tdd/StringCalculator/tests/StringCalculatorTest.php
+++ tdd/StringCalculator/tests/StringCalculatorTest.php
@@ -13,7 +13,7 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
         $this->stringCalculator = new StringCalculator();
     }
     
-    // tests
+    //tests
     public function testIfEmptyStringReturnsZero() {
         $this->assertEquals(0, $this->stringCalculator->add(""));
     }
@@ -21,81 +21,87 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, $this->stringCalculator->add("1"));
         $this->assertEquals(2, $this->stringCalculator->add("2"));
         $this->assertEquals(3, $this->stringCalculator->add("3"));
-    }		
-    public function testReturnSumOfTwoNumbers()
-    {
-    	$this->assertEquals(1, $this->stringCalculator->add("0,1"));
-    	$this->assertEquals(2, $this->stringCalculator->add("1,1"));
-    	$this->assertEquals(3, $this->stringCalculator->add("1,2"));
-    	$this->assertNotEquals(3, $this->stringCalculator->add("2,2"));
-    	$this->assertEquals(16, $this->stringCalculator->add("9,7"));
-    	$this->assertEquals(20, $this->stringCalculator->add("9,11"));
-    	$this->assertEquals(40, $this->stringCalculator->add("10,30"));
-    	$this->assertEquals(160, $this->stringCalculator->add("100,60"));
+    }
+    public function testReturnSumOfTwoNumbers() {
+        $this->assertEquals(1, $this->stringCalculator->add("0,1"));
+        $this->assertEquals(2, $this->stringCalculator->add("1,1"));
+        $this->assertEquals(3, $this->stringCalculator->add("1,2"));
+        $this->assertNotEquals(3, $this->stringCalculator->add("2,2"));
+        $this->assertEquals(16, $this->stringCalculator->add("9,7"));
+        $this->assertEquals(20, $this->stringCalculator->add("9,11"));
+        $this->assertEquals(40, $this->stringCalculator->add("10,30"));
+        $this->assertEquals(160, $this->stringCalculator->add("100,60"));
     }
     
-    public function testIfSumsUnknouwnAmountOfNumbers()
-    {
-    	$this->assertEquals(1, $this->stringCalculator->add("0,0,1"));
-    	$this->assertEquals(2, $this->stringCalculator->add("0,1,1"));
-    	$this->assertEquals(3, $this->stringCalculator->add("1,1,1"));
-    	$this->assertEquals(4, $this->stringCalculator->add("2,1,1"));
-    	$this->assertEquals(24, $this->stringCalculator->add("3,10,11"));
-    	$this->assertEquals(34, $this->stringCalculator->add("3,9,10,12"));
-    	$this->assertEquals(110, $this->stringCalculator->add("5,5,10,12,20,28,30"));
+    public function testIfSumsUnknouwnAmountOfNumbers() {
+        $this->assertEquals(1, $this->stringCalculator->add("0,0,1"));
+        $this->assertEquals(2, $this->stringCalculator->add("0,1,1"));
+        $this->assertEquals(3, $this->stringCalculator->add("1,1,1"));
+        $this->assertEquals(4, $this->stringCalculator->add("2,1,1"));
+        $this->assertEquals(24, $this->stringCalculator->add("3,10,11"));
+        $this->assertEquals(34, $this->stringCalculator->add("3,9,10,12"));
+        $this->assertEquals(110, $this->stringCalculator->add("5,5,10,12,20,28,30"));
     }
-    public function testIfSumsUnknownAmountOfnumbersWithNewLinesAndCommasAsSeparators()
-    {
-    	$this->assertEquals(6, $this->stringCalculator->add("1\n2,3"));
-    	$this->assertEquals(6, $this->stringCalculator->add("1,2\n3"));
+    public function testIfSumsUnknownAmountOfnumbersWithNewLinesAndCommasAsSeparators() {
+        $this->assertEquals(6, $this->stringCalculator->add("1\n2,3"));
+        $this->assertEquals(6, $this->stringCalculator->add("1,2\n3"));
         $this->assertEquals(6, $this->stringCalculator->add("1\n2\n3"));
-    	$this->assertNotEquals(6, $this->stringCalculator->add("2\n2\n3"));
+        $this->assertNotEquals(6, $this->stringCalculator->add("2\n2\n3"));
     }
-    public function testIfCanGetDelimiter()
-    {
-        $this->assertEquals(3, $this->stringCalculator->add("//;\n1;2"));
-        $this->assertEquals(4, $this->stringCalculator->add("//,\n2,2"));
-        $this->assertEquals(5, $this->stringCalculator->add("//|\n3|2"));
+    public function testIfCanGetDelimiter() {
+        $this->assertEquals(3, $this->stringCalculator->add("//[;]\n1;2"));
+        $this->assertEquals(4, $this->stringCalculator->add("//[,]\n2,2"));
+        $this->assertEquals(5, $this->stringCalculator->add("//[|]\n3|2"));
     }
+    
     /**
      * @expectedException InvalidArgumentException
      * @expectedExceptionMessage negatives not allowed -1
      */
-    public function testIfWrongNegativeNumbersWithComa()
-    {
-         $this->stringCalculator->add("0,1,-1");
+    public function testIfWrongNegativeNumbersWithComa() {
+        $this->stringCalculator->add("0,1,-1");
     }
+    
     /**
      * @expectedException InvalidArgumentException
      * @expectedExceptionMessage negatives not allowed -2
      */
-    public function testIfWrongNegativeNumbersWithSep()
-    {
-         $this->stringCalculator->add("//;\n3;-2");
+    public function testIfWrongNegativeNumbersWithSep() {
+        $this->stringCalculator->add("//[;]\n3;-2");
     }
+    
     /**
      * @expectedException InvalidArgumentException
      * @expectedExceptionMessage negatives not allowed -2
      */
-    public function testIfWrongNegativeNumbersSpaceAndComma()
-    {
-         $this->stringCalculator->add("1\n3,-2");
+    public function testIfWrongNegativeNumbersSpaceAndComma() {
+        $this->stringCalculator->add("1\n3,-2");
     }
-
-     /**
+    
+    /**
      * @expectedException InvalidArgumentException
      * @expectedExceptionMessage negatives not allowed -1,-2,-3
      */
-    public function testIfShowsMoreThenOneNegativeNumberInExeception()
-    {
+    public function testIfShowsMoreThenOneNegativeNumberInExeception() {
         $this->stringCalculator->add("-1,-2,-3");
     }
-     /**
+    
+    /**
      * @expectedException InvalidArgumentException
      * @expectedExceptionMessage negatives not allowed -1,-3
      */
-    public function testIfShowsMoreThenOneNegativeNumberWithSep()
-    {
+    public function testIfShowsMoreThenOneNegativeNumberWithSep() {
         $this->stringCalculator->add("-1\n0,-3");
+    }
+    public function testIfRefusesNumbersGt100() {
+        $this->assertEquals(2, $this->stringCalculator->add("//[;]\n1001;2"));
+        $this->assertEquals(1000, $this->stringCalculator->add("//[;]\n999;1"));
+        $this->assertEquals(1001, $this->stringCalculator->add("1000,1"));
+    }
+    public function testIfDelimiterCanBenOfAnyLength()
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("//[***]\n1***2***3"));
+        $this->assertEquals(3, $this->stringCalculator->add("//[---]\n1---2"));
+    
     }
 }
